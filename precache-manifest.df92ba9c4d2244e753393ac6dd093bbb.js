1987caf4a982cf3a0fb796f38da1617bc471a265
Taras Golyk <tarasgolyk87@gmail.com>
2020-03-23 17:08:34 +0200

    Updates

---
+++ precache-manifest.df92ba9c4d2244e753393ac6dd093bbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5bbbc7ea6c6e764fd5d65ad4f4b4a5c",
+    "url": "/hokage_telegram_bot/index.html"
+  },
+  {
+    "revision": "768711e98d799685933f",
+    "url": "/hokage_telegram_bot/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0b5478d0462a3dd6d2d3",
+    "url": "/hokage_telegram_bot/static/css/main.b0edf969.chunk.css"
+  },
+  {
+    "revision": "768711e98d799685933f",
+    "url": "/hokage_telegram_bot/static/js/2.56ffb167.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hokage_telegram_bot/static/js/2.56ffb167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5478d0462a3dd6d2d3",
+    "url": "/hokage_telegram_bot/static/js/main.67183b0c.chunk.js"
+  },
+  {
+    "revision": "29a63a3aaab53c7899f2",
+    "url": "/hokage_telegram_bot/static/js/runtime-main.2aa6bca1.js"
+  },
+  {
+    "revision": "1f7dbb0611382196f5d15c1eb009e24b",
+    "url": "/hokage_telegram_bot/static/media/back.1f7dbb06.jpg"
+  }
+]);
